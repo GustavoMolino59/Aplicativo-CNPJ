24dfe9e23e699382a9998688e82611062b7a3d60
Erick Felino <erick.felino@consultoriakaizen.net>
2023-11-13 22:41:12 -0300

    [IMP] reafatoração do código

--- Appcnoj.py
+++ Appcnoj.py
@@ -1,25 +1,24 @@
 import pandas as pd
 import requests
-import os
 import time
 
-file_path = os.path.abspath('fundoIdosos.xlsx')
-df = pd.read_excel("CNPJ.xlsx")
-print(df.columns) #Teste
-for i in range (0,100):
-    dado = df["CNPJ"].iloc[i] 
+planilha = pd.read_csv("FDCA2023.csv", sep=';', encoding='ISO-8859-1')
+
+planilha = planilha.drop(planilha.index[:2])
+planilha = planilha.reset_index(drop=True)
+planilha.columns = planilha.iloc[0]
+planilha = planilha.drop(planilha.index[0])
+
+planilha = planilha.assign(nome='')
+
+for index, row in planilha.iterrows():
+    dado = row["CNPJ"]
     url = 'https://receitaws.com.br/v1/cnpj/' + str(dado)
     response = requests.get(url)
     while response.status_code != 200:
         time.sleep(20)
         response = requests.get(url)
     valor_obtido = response.json()
-    Cnpj_temporario = pd.DataFrame([valor_obtido])
-    if i == 0:
-        CNPJ_ATUAL = pd.DataFrame([valor_obtido])
-    else:
-        CNPJ_ATUAL = pd.concat([CNPJ_ATUAL, Cnpj_temporario], ignore_index=False)
-writer = pd.ExcelWriter('Teste.xlsx', engine='xlsxwriter') #Cada vez que rodar o código precisamos mudar a planilha senão apaga oa antigo
-print(CNPJ_ATUAL.iloc[0,0]) #Teste
-CNPJ_ATUAL.to_excel(excel_writer=writer, sheet_name='Plan1', index=True, startrow= 2, )
-writer.close()
+    planilha.at[index, 'nome'] =  valor_obtido.get("nome")
+
+planilha.to_csv("Teste.csv", sep=",", index=False)
